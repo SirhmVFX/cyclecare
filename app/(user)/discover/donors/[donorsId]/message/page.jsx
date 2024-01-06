b7795816cd540894b4d5483087d36da40b1b1610
sirhmvfx <sirhmvfx@gmail.com>
2024-01-06 02:16:31 +0100

    working on routes

--- app/(user)/discover/donors/[donorsId]/message/page.jsx
+++ app/(user)/discover/donors/[donorsId]/message/page.jsx
@@ -5,11 +5,11 @@ function Message() {
     <>
       <section className="w-full md:w-2/4 mx-auto p-8">
         <div className="flex justify-between">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center gap-3">
             <Image
               src="/images/user.png"
-              width={50}
-              height={50}
+              width={40}
+              height={40}
               alt="user"
               className="border rounded-full border-rose-600"
             />
