a01b366f448babab7bc96fc41a0e76c71cdee060
sirhmvfx <sirhmvfx@gmail.com>
2024-01-25 18:01:42 +0100

    done

--- app/(user)/discover/aichat/page.jsx
+++ app/(user)/discover/aichat/page.jsx
@@ -33,7 +33,8 @@ function Message() {
             <p className="font-bold">Cyclevcare AI Assitant</p>
             <div className="bg-gray-200 py-2 px-4 rounded-lg w-3/4 ">
               <p className="text-sm text-gray-400">
-                Hello {user?.email}, How may i be of help to you today
+                Hello {user?.email.split("@")[0]}, How may i be of help to you
+                today
               </p>
             </div>
           </div>
